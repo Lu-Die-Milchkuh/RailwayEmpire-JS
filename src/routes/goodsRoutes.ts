218c1532b5c08da992496e81803805b2eed63a10
Lu-Die-Milchkuh <78442275+Lu-Die-Milchkuh@users.noreply.github.com>
2023-11-23 20:11:25 +0100

    Initial Implementation of Goods Routes

--- src/routes/goodsRoutes.ts
+++ src/routes/goodsRoutes.ts
@@ -1,5 +1,30 @@
-import Elysia from "elysia"
+import { Elysia, t } from "elysia"
+import GoodsController from "../controller/goodsController"
 
-const goodsRoutesPlugin = new Elysia({ prefix: "/good" }).get("/", () => {})
+const goodsController = new GoodsController()
+
+const goodsRoutesPlugin = new Elysia({ prefix: "/goods" })
+    // Schema of the required body
+    .model({
+        Good: t.Object({
+            type: t.String(),
+            amount: t.Number()
+        })
+    })
+    .get("/", goodsController.getAllGoods)
+    .guard(
+        {
+            body: "Good",
+            error({}) {
+                return {
+                    error: "Type and Amount expected!"
+                }
+            }
+        },
+        (app) =>
+            app
+                .get("/buy", goodsController.buyGood)
+                .post("/sell", goodsController.sellGood)
+    )
 
 export default goodsRoutesPlugin
