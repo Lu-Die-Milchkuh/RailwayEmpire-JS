a8ed402b29bc5a7287db89ee0fead517045d288d
Lu-Die-Milchkuh <78442275+Lu-Die-Milchkuh@users.noreply.github.com>
2023-11-24 15:16:58 +0100

    Implement Login and Registration

--- src/controller/publicController.ts
+++ src/controller/publicController.ts
@@ -1,10 +1,61 @@
 class PublicController {
     async register(ctx) {
         const db = ctx.db
+        const username = ctx.body.username
+        const password = ctx.body.password
+        const jwt = ctx.jwt
+
+        try {
+            const result = await db.register(username, password)
+
+            if (!result) {
+                ctx.set.status = 400
+                return {
+                    error: "Username already taken!"
+                }
+            }
+
+            const token = await jwt.sign(ctx.body)
+            return {
+                token: token
+            }
+        } catch (error) {
+            console.log(error)
+            ctx.set.status = 500
+            return {
+                error: "Internal Server error! Please try again later."
+            }
+        }
     }
 
     async login(ctx) {
-        console.log(ctx.body)
+        const db = ctx.db
+        const username = ctx.body.username
+        const password = ctx.body.password
+        const jwt = ctx.jwt
+
+        try {
+            const result = await db.login(username, password)
+
+            if (!result) {
+                ctx.set.status = 404
+                return {
+                    error: "Username not found!"
+                }
+            }
+
+            const token = await jwt.sign(ctx.body)
+
+            return {
+                token: token
+            }
+        } catch (error) {
+            console.log(error)
+            ctx.set.status = 500
+            return {
+                error: "Internal Server error! Please try again later."
+            }
+        }
     }
 
     // Return an array of all World containing the ID,CreationDate and the number of Players
