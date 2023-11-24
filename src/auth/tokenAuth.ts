b153075aafa07cd89338e992b3d64f3246289723
Lu-Die-Milchkuh <78442275+Lu-Die-Milchkuh@users.noreply.github.com>
2023-11-24 15:16:04 +0100

    Initial Implementation of Token Validation Middleware

--- src/auth/tokenAuth.ts
+++ src/auth/tokenAuth.ts
@@ -1,1 +1,22 @@
-async function validateToken(ctx) {}
+export default async function validateToken(ctx) {
+    const jwt = ctx.jwt
+    const token = ctx.headers["authorization"]
+
+    console.log("Token: " + token)
+
+    if (!token) {
+        ctx.set.status = 401
+        return {
+            error: "Unauthorized! Missing Token!"
+        }
+    }
+
+    const profile = await jwt.verify(token, Bun.env.JWT_SECRET)
+
+    if (!profile) {
+        ctx.set.status = 401
+        return {
+            error: "Unauthorized! The provided Token is not valid!"
+        }
+    }
+}
