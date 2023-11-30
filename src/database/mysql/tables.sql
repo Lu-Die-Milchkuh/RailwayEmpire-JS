b6411c76635f4a9f058f6c1ae1aa78526c5bf618
Lu-Die-Milchkuh <78442275+Lu-Die-Milchkuh@users.noreply.github.com>
2023-11-30 12:58:26 +0100

    Pushing to latest Version

--- src/database/mysql/tables.sql
+++ src/database/mysql/tables.sql
@@ -30,7 +30,48 @@ CREATE TABLE
     );
 
 CREATE TABLE
+    Good(
+        goodID INT AUTO_INCREMENT PRIMARY KEY,
+        type ENUM('GRAIN', 'BEVERAGE', 'WOOD', 'MEAT', 'MILK', 'BREAD', 'FRUIT', 'PLANKS', 'LEATHER', 'WOOL', 'CHEESE', 'FURNITURE', 'CLOTHING', 'METALS', 'JEWELLERY', 'TOOLS') NOT NULL,
+        amount INT NOT NULL
+    );
+
+CREATE TABLE
+    Asset(
+        assetID INT AUTO_INCREMENT PRIMARY KEY,
+        type ENUM('RANCH', 'FIELD', 'STATION', 'DEPOT', 'RAIL') NOT NULL,
+        name VARCHAR(255),
+        population INT DEFAULT 0,
+        position POINT NOT NULL,
+        userIDFK INT
+    );
+
+CREATE TABLE
     Town(
         townID INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        level INT NOT NULL DEFAULT 1,
+        position POINT NOT NULL,
+        population INT NOT NULL,
+        userIDFK INT ,
+        worldIDFK INT NOT NULL,
+        FOREIGN KEY (worldIDFK) REFERENCES World(worldID),
+        FOREIGN KEY (userIDFK) REFERENCES User(userID)
+    );
 
-)
+CREATE TABLE
+    Station(
+        stationID INT AUTO_INCREMENT PRIMARY KEY,
+        townIDFK INT NOT NULL,
+        FOREIGN KEY (townIDFK) REFERENCES Town(townID)
+    );
+
+CREATE TABLE
+    Track(
+        trackID INT AUTO_INCREMENT PRIMARY KEY,
+        stationID1FK INT NOT NULL,
+        stationID2FK INT NOT NULL,
+        FOREIGN KEY (stationID1FK) REFERENCES Station(stationID),
+        FOREIGN KEY (stationID2FK) REFERENCES Station(stationID)
+    );
+
