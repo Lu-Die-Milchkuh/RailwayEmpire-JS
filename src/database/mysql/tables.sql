b6f3d3a63997fc381ea17f1b5d8a39c8416108f5
Lu-Die-Milchkuh <78442275+Lu-Die-Milchkuh@users.noreply.github.com>
2023-12-01 13:22:58 +0100

    Pushing to latest Version

--- src/database/mysql/tables.sql
+++ src/database/mysql/tables.sql
@@ -5,73 +5,132 @@ CREATE DATABASE IF NOT EXISTS Railway;
 USE Railway;
 
 CREATE TABLE
-    World (
-        worldID INT AUTO_INCREMENT PRIMARY KEY,
-        creationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        numberOfPlayers INT NOT NULL
-    );
+    World
+(
+    worldID         INT AUTO_INCREMENT PRIMARY KEY,
+    creationDate    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    numberOfPlayers INT NOT NULL
+);
 
 CREATE TABLE
-    User (
-        userID INT AUTO_INCREMENT PRIMARY KEY,
-        username VARCHAR(255) UNIQUE NOT NULL,
-        password VARCHAR(255) NOT NULL,
-        joinDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        worldIDFK INT,
-        FOREIGN KEY (worldIDFK) REFERENCES World(worldID)
-    );
+    User
+(
+    userID    INT AUTO_INCREMENT PRIMARY KEY,
+    username  VARCHAR(255) UNIQUE NOT NULL,
+    password  VARCHAR(255)        NOT NULL,
+    funds     FLOAT       DEFAULT 100000.0,
+    joinDate  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    worldIDFK INT,
+    FOREIGN KEY (worldIDFK) REFERENCES World (worldID)
+);
 
 CREATE TABLE
-    Token(
-        tokenID INT AUTO_INCREMENT PRIMARY KEY,
-        userIDFK INT NOT NULL,
-        token VARCHAR(512) NOT NULL,
-        FOREIGN KEY (userIDFK) REFERENCES User(userID)
-    );
+    Token
+(
+    tokenID  INT AUTO_INCREMENT PRIMARY KEY,
+    userIDFK INT          NOT NULL,
+    token    VARCHAR(512) NOT NULL,
+    FOREIGN KEY (userIDFK) REFERENCES User (userID)
+);
 
 CREATE TABLE
-    Good(
-        goodID INT AUTO_INCREMENT PRIMARY KEY,
-        type ENUM('GRAIN', 'BEVERAGE', 'WOOD', 'MEAT', 'MILK', 'BREAD', 'FRUIT', 'PLANKS', 'LEATHER', 'WOOL', 'CHEESE', 'FURNITURE', 'CLOTHING', 'METALS', 'JEWELLERY', 'TOOLS') NOT NULL,
-        amount INT NOT NULL
-    );
+    Good
+(
+    goodID INT AUTO_INCREMENT PRIMARY KEY,
+    type   ENUM ('MAIL','PASSENGER','FRUIT','WHEAT','CATTLE','GRAIN','WOOD','MILK','PLANKS','LEATHER','WOOL','ORE','TOOLS','GEMS','BEVERAGE','MEAT','BREAD','CHEESE','FURNITURE','CLOTHING','METALS','JEWELLERY') NOT NULL,
+    amount INT                                                                                                                                                                                                                      NOT NULL,
+    price  FLOAT                                                                                                                                                                                                                      NOT NULL
+);
 
 CREATE TABLE
-    Asset(
-        assetID INT AUTO_INCREMENT PRIMARY KEY,
-        type ENUM('RANCH', 'FIELD', 'STATION', 'DEPOT', 'RAIL') NOT NULL,
-        name VARCHAR(255),
-        population INT DEFAULT 0,
-        position POINT NOT NULL,
-        userIDFK INT
-    );
+    Asset
+(
+    assetID    INT AUTO_INCREMENT PRIMARY KEY,
+    type       ENUM ('TOWN', 'RANCH', 'FIELD', 'FARM', 'LUMBERYARD','PLANTATION','MINE') NOT NULL,
+    name       VARCHAR(255),
+    population INT DEFAULT 0,
+    position   POINT                                                                     NOT NULL,
+    level      INT DEFAULT 1,
+    cost      FLOAT NOT NULL ,
+    costPerDay FLOAT NOT NULL,
+    userIDFK   INT,
+    FOREIGN KEY (userIDFK) REFERENCES User (userID)
+);
 
 CREATE TABLE
-    Town(
-        townID INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        level INT NOT NULL DEFAULT 1,
-        position POINT NOT NULL,
-        population INT NOT NULL,
-        userIDFK INT ,
-        worldIDFK INT NOT NULL,
-        FOREIGN KEY (worldIDFK) REFERENCES World(worldID),
-        FOREIGN KEY (userIDFK) REFERENCES User(userID)
-    );
+    Station
+(
+    stationID INT AUTO_INCREMENT PRIMARY KEY,
+    assetIDFK INT NOT NULL,
+    cost      FLOAT DEFAULT 100000,
+    costPerDay FLOAT DEFAULT (1000.0/7.0),
+    FOREIGN KEY (assetIDFK) REFERENCES Asset (assetID)
+);
 
 CREATE TABLE
-    Station(
-        stationID INT AUTO_INCREMENT PRIMARY KEY,
-        townIDFK INT NOT NULL,
-        FOREIGN KEY (townIDFK) REFERENCES Town(townID)
-    );
+    Track
+(
+    trackID      INT AUTO_INCREMENT PRIMARY KEY,
+    stationID1FK INT NOT NULL,
+    stationID2FK INT NOT NULL,
+    FOREIGN KEY (stationID1FK) REFERENCES Station (stationID),
+    FOREIGN KEY (stationID2FK) REFERENCES Station (stationID)
+);
 
 CREATE TABLE
-    Track(
-        trackID INT AUTO_INCREMENT PRIMARY KEY,
-        stationID1FK INT NOT NULL,
-        stationID2FK INT NOT NULL,
-        FOREIGN KEY (stationID1FK) REFERENCES Station(stationID),
-        FOREIGN KEY (stationID2FK) REFERENCES Station(stationID)
-    );
+    Industry
+(
+    industryID INT AUTO_INCREMENT PRIMARY KEY,
+    type       ENUM (
+        'BREWERY', 'BUTCHER', 'BAKERY', 'SAWMILL', 'CHEESEMAKER', 'CARPENTER','TAILOR',
+        'SMELTER', 'SMITHY', 'JEWELER'
+        )          NOT NULL,
+    assetIDFK  INT NOT NULL,
+    FOREIGN KEY (assetIDFK) REFERENCES Asset (assetID)
+);
+
+CREATE TABLE
+    Consumes
+(
+    goodIDFK     INT NOT NULL,
+    industryIDFK INT,
+    assetIDFK    INT,
+    amount       INT NOT NULL,
+    FOREIGN KEY (goodIDFK) REFERENCES Good (goodID),
+    FOREIGN KEY (industryIDFK) REFERENCES Industry (industryID),
+    FOREIGN KEY (assetIDFK) REFERENCES Asset (assetID)
+);
+
+CREATE TABLE
+    Produces
+(
+    goodIDFK     INT NOT NULL,
+    industryIDFK INT,
+    assetIDFK    INT,
+    amount       INT NOT NULL,
+    FOREIGN KEY (goodIDFK) REFERENCES Good (goodID),
+    FOREIGN KEY (industryIDFK) REFERENCES Industry (industryID),
+    FOREIGN KEY (assetIDFK) REFERENCES Asset (assetID)
+);
+
+CREATE TABLE
+    Train
+(
+    trainID    INT AUTO_INCREMENT PRIMARY KEY,
+    assetIDFK  INT NOT NULL,
+    cost INT DEFAULT 50000,
+    costPerDay INT DEFAULT 500,
+    FOREIGN KEY (assetIDFK) REFERENCES Asset (assetID)
+);
+
+CREATE TABLE
+    Wagon
+(
+    wagonID    INT AUTO_INCREMENT PRIMARY KEY,
+    trainIDFK  INT NOT NULL,
+    goodIDFK   INT NOT NULL,
+    amount     INT NOT NULL,
+    FOREIGN KEY (trainIDFK) REFERENCES Train (trainID),
+    FOREIGN KEY (goodIDFK) REFERENCES Good (goodID)
+);
 
