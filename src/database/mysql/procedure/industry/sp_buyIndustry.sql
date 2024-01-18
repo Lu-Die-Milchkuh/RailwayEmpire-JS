e64e2e8b0e17cc1d49bd760b7c8a1318e12bbd05
Lu-Die-Milchkuh <78442275+Lu-Die-Milchkuh@users.noreply.github.com>
2024-01-18 15:21:33 +0100

    Fixed a shitty Mysql Bug. I hate it so much

--- src/database/mysql/procedure/industry/sp_buyIndustry.sql
+++ src/database/mysql/procedure/industry/sp_buyIndustry.sql
@@ -4,7 +4,7 @@ DELIMITER $$
 
 DROP PROCEDURE IF EXISTS sp_buyIndustry;
 
-CREATE PROCEDURE sp_buyIndustry(IN jsonData JSON)
+CREATE PROCEDURE sp_buyIndustry(IN p_jsonData JSON)
 sp:
 BEGIN
     DECLARE v_userID INT;
@@ -17,27 +17,44 @@ BEGIN
         );
 
     DECLARE v_schema JSON;
-    DECLARE v_data JSON;
+    DECLARE temp_userID INT;
+    -- DECLARE v_data JSON;
 
+    /* Mysql sucks so hard, "type" is a reserved keyword (that is why I use type1)
+       I spend so much time debugging this shit, because the json validation
+       failed even with valid json. I hate it so much. Why cant they just provide proper errors?!?!?
+    */
     SET v_schema = '{
       "userID": "integer",
       "assetID": "integer",
-      "type": "string"
+      "type1": "string"
     }';
 
-    -- IF NOT JSON_VALID(jsonData) OR NOT JSON_SCHEMA_VALID(v_schema, jsonData) THEN
-    --    SIGNAL SQLSTATE '45000'
-    --        SET MESSAGE_TEXT = 'JSON is not valid';
-    -- END IF;
+    IF NOT JSON_VALID(p_jsonData) OR NOT JSON_SCHEMA_VALID(v_schema, p_jsonData) THEN
+        SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'JSON is not valid';
+    END IF;
 
-    SET v_townID = JSON_UNQUOTE(JSON_EXTRACT(jsonData, '$.assetID'));
-    SET v_industryType = JSON_UNQUOTE(JSON_EXTRACT(jsonData, '$.type'));
-    SET v_userID = JSON_UNQUOTE(JSON_EXTRACT(jsonData, '$.userID'));
+    SET v_townID = JSON_UNQUOTE(JSON_EXTRACT(p_jsonData, '$.assetID'));
+    SET v_industryType = JSON_UNQUOTE(JSON_EXTRACT(p_jsonData, '$.type1'));
+    SET v_userID = JSON_UNQUOTE(JSON_EXTRACT(p_jsonData, '$.userID'));
 
     IF v_townID IS NULL OR v_industryType IS NULL OR v_userID IS NULL THEN
         SELECT JSON_OBJECT(
-                       'code', 400,
+                       'code', 401,
                        'message', 'Missing parameters',
+                       'data', null
+               ) as output;
+        LEAVE sp;
+    END IF;
+
+    -- Check if the User is the owner of the Town
+    SELECT userIDFK INTO temp_userID FROM Asset WHERE assetID = v_townID LIMIT 1;
+
+    IF temp_userID IS NULL OR temp_userID != v_userID THEN
+        SELECT JSON_OBJECT(
+                       'code', 401,
+                       'message', 'User is not the owner of the Town',
                        'data', null
                ) as output;
         LEAVE sp;
@@ -57,9 +74,9 @@ BEGIN
         LEAVE sp;
     END IF;
 
-    UPDATE User SET funds = v_funds - v_industryCost WHERE userID = v_userID;
+    UPDATE User SET funds = funds - v_industryCost WHERE userID = v_userID;
 
-    INSERT INTO Industry(assetIDFK,type) VALUES (v_townID,v_industryType);
+    INSERT INTO Industry(assetIDFK, type) VALUES (v_townID, v_industryType);
 
     SELECT JSON_OBJECT(
                    'code', 200,
@@ -69,6 +86,3 @@ BEGIN
 END$$
 
 DELIMITER ;
-
--- CALL sp_buyIndustry('{"userID":101,"assetID":2227,"type":"BUTCHER"}');
-
